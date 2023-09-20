77a2ff46411511cc4190b4c8e846270f87834374
Spoonail-Iroiro <example@example.com>
2023-09-20 16:09:10 +0900

    Change interrupt target to viewSkill
    Key click handler should be non-modal.
    To do so, it's easier to patch viewSkill to be disabled
    during custom key handling than to patch overlook.

--- js/mod/mods/qsave/init.js
+++ js/mod/mods/qsave/init.js
@@ -3,97 +3,141 @@ import maginai from 'maginai';
 const logger = maginai.logging.getLogger('plustalk');
 const pt = maginai.patcher;
 
-function save() {
-  const gm = tWgm;
-  gm.tGameSave.save(function (h) {
-    if (h) {
-      gm.tGameLog.addAndViewLog(
-        gm.tGameTalkResource.talkData.system.save_ok,
-        false
-      );
-    } else {
-      gm.tGameLog.addAndViewLog(
-        gm.tGameTalkResource.talkData.system.save_ng,
-        false
-      );
-    }
-  });
-}
+let isViewAbilityBlocked = false;
 
 class ShortcutKeyInterrupter {
-  _isDash;
-  isOrigKeyProcessDisabled = false;
-
-  disableOrigKeyProcessUntilNextIsActionFalse() {
-    this.isOrigKeyProcessDisabled = true;
+  constructor() {
+    /** @type {Record<string, Function>}*/
+    this.handlers = {};
+  }
+  blockViewAbility() {
+    isViewAbilityBlocked = true;
   }
 
-  patchIsDash(routineMap) {
-    if (routineMap === undefined) {
-      throw new Error('tGameRoutineMap is not loaded');
-    }
-    this._isDash = routineMap.player.isDash;
-    delete routineMap.player.isDash;
-    const self = this;
-    Object.defineProperty(routineMap.player, 'isDash', {
-      set: function (value) {
-        if (value === false) self.isOrigKeyProcessDisabled = false;
-        self._isDash = value;
-      },
-      get: function () {
-        return self._isDash;
-      },
+  end() {
+    const grm = tWgm.tGameRoutineMap;
+    isViewAbilityBlocked = false;
+    grm.setFrameAction(tWgm);
+    grm.player.isDash = false;
+    grm.isAction = false;
+  }
+
+  /**
+   * 追加キー処理のため無効化していたtGameMenu.viewSkillを有効化する
+   * 追加キー処理の中でviewSkillにアクセスしたいときに呼ぶ
+   */
+  recoverViewAbility() {
+    isViewAbilityBlocked = false;
+  }
+
+  patchViewSkill() {
+    pt.patchMethod(tGameMenu, 'viewSkill', (origMethod) => {
+      const rtnFn = function (...args) {
+        if (isViewAbilityBlocked) return;
+
+        return origMethod.call(this, ...args);
+      };
+      return rtnFn;
     });
+  }
+
+  /**
+   * 追加キーのclickイベントハンドラの登録
+   * @param {string} keyCode
+   * @param {(end: () => void, recoverViewAbility: () => void) =>void} fn ハンドラー
+   *    処理の終わりに必ず第一引数のend()関数を呼ぶべき（呼ばない場合フリーズ）
+   */
+  addHandler(keyCode, fn) {
+    const keyList = ['f1', 'f2', 'f3', 'f4'];
+    if (!keyList.includes(keyCode)) {
+      throw new Error(`キーは次のうちの1つである必要があります: [${keyList}]`);
+    }
+    this.handlers[keyCode] = fn;
   }
 
   patchIsClick() {
     const self = this;
     pt.patchMethod(tGameKeyboard, 'isClick', (origMethod) => {
-      const rtnFn = function (keyCode, ...rest) {
-        if (keyCode === 'command_miwatasu') {
-          const isf1Clicked = origMethod.call(this, 'f1', ...rest);
-          if (isf1Clicked) {
-            logger.debug('f1 clicked!');
-            self.onF1Clicked();
-            self.disableOrigKeyProcessUntilNextIsActionFalse();
-            return true;
-          } else {
-            return origMethod.call(this, keyCode, ...rest);
+      const rtnFn = function (origKeyCode, ...rest) {
+        if (origKeyCode === 'command_ability') {
+          for (const keyCode of Object.keys(self.handlers)) {
+            const clicked = origMethod.call(this, keyCode, ...rest);
+            if (clicked) {
+              logger.debug(`${keyCode} clicked!`);
+              const fn = self.handlers[keyCode];
+              // 非同期処理のため必ずPromiseで実行
+              Promise.resolve().then(() => {
+                fn(self.end);
+              });
+              // 本来のviewAbilityをブロックする
+              // handlerがself.end()を適切に呼ぶことでキー受付のブロック解除とともに解除される
+              self.blockViewAbility();
+              return true;
+            }
           }
-        } else {
-          return origMethod.call(this, keyCode, ...rest);
         }
+        // 追加キーで処理が終わらないか、相乗りするコマンドでなければ元のメソッドの結果を返す
+        return origMethod.call(this, origKeyCode, ...rest);
       };
       return rtnFn;
     });
   }
+}
 
-  patchSetModeOverlook() {
-    const self = this;
-    pt.patchMethod(tGameRoutineMap, 'setMode', (origMethod) => {
-      const rtnFn = function (mode, ...rest) {
-        if (self.isOrigKeyProcessDisabled && mode === 'overlook') return;
+function isSaveable() {
+  let isSaveEnable = true;
+  const viewMapInfo = tWgm.tGameData.getDataValue('viewMapInfo');
 
-        return origMethod.call(this, mode, ...rest);
-      };
-      return rtnFn;
+  // nullの可能性もあるので!=
+  if (viewMapInfo?.isSaveEnable != undefined) {
+    isSaveEnable = viewMapInfo.isSaveEnable;
+  }
+
+  const islocked = tWgm.tGameData.getDataValue('lock')[0];
+  return !islocked && isSaveEnable;
+}
+
+function save(end) {
+  try {
+    const gm = tWgm;
+    if (!isSaveable()) {
+      gm.tGameLog.addAndViewLog('ここではセーブできない。', false);
+      end();
+      return;
+    }
+
+    gm.tGameSave.save(function (isOk) {
+      try {
+        if (isOk) {
+          gm.tGameLog.addAndViewLog(
+            gm.tGameTalkResource.talkData.system.save_ok,
+            false
+          );
+        } else {
+          gm.tGameLog.addAndViewLog(
+            gm.tGameTalkResource.talkData.system.save_ng,
+            false
+          );
+        }
+      } finally {
+        end();
+      }
     });
+  } catch (e) {
+    logger.error(e);
+    end();
   }
+}
 
-  onF1Clicked() {
-    maginai.logToInGameLogDebug('F1 clicked!');
-    save();
-  }
+function onF1Clicked(end) {
+  save(end);
 }
 
 let intr = new ShortcutKeyInterrupter();
 
 intr.patchIsClick();
-intr.patchSetModeOverlook();
+intr.patchViewSkill();
 
-maginai.events.gameLoadFinished.addHandler(() => {
-  // isActionメンバーにパッチするのでtWgmが読み込まれてから
-  intr.patchIsDash(tWgm.tGameRoutineMap);
-});
+intr.addHandler('f1', onF1Clicked);
 
 export default intr;
